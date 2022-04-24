5efeec34a4881e0aaa09d10a899a13ce0d9625e7
Clinton Renton <93984913+YonTracks@users.noreply.github.com>
2022-04-24 13:31:44 +1000

    First commit

---
+++ Maps/desert_Pack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="tmw_desert_spacing" tilewidth="12" tileheight="12" spacing="1" margin="1" tilecount="1024" columns="32">
+ <image source="../../../../Program Files/Tiled/examples/tmw_desert_spacing.png" width="384" height="384"/>
+</tileset>
